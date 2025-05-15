2e2b09e85233deba2229901694ab4f5508a2ad77
Anthony <102490646+Drakeze@users.noreply.github.com>
2025-05-14 17:07:08 -0700

    Update Oop.py

--- Code/Python/Basics(PY)/PracticeCode/Oop.py
+++ Code/Python/Basics(PY)/PracticeCode/Oop.py
@@ -6,16 +6,30 @@ class BankAccount:
 # self: refers to the instance of the class being created
 # account_number: a unique identifier for the account
 # initial_balance: the initial balance of the account (default is 0)
-def __init__(self, account_number, initial_balance=0):
+def __init__(self, account_number, initial_balance=0, account_type="Active"):
     self.account_number = account_number
     self.balance = initial_balance
     account_number = len(BankAccount.accounts) + 1001
     BankAccount.accounts[account_number] = self
 # This is for account information
 def __str__(self):
-    return f"Account Number: {self.account_number}, Balance: ${self.balance} ,Account Type: {self.account_type}"
+    account_info {
+        f"Account Number: {self.account_number}",
+        f"Account Type: {self.account_type}",
+        f"Balance: ${self.balance}"
+        f"Status: {'Active' if self.account_number in BankAccount.accounts else 'Inactive'}"
+        else:
+        "closed"
+    }
+    return str (account_info)
 #this is for changing account type active or inactive
-def change_account_type()
+def change_account_type( self, change_type):
+    if change_type.lower() == "active":
+        self.account_type = "Active"
+    elif change_type.lower() == "inactive":
+        self.account_type = "Inactive"
+    else:
+        print("Invalid account type. Please choose 'active' or 'inactive'.")
 #This is for multiple accounts
 MAX_ACCOUNTS = 3
 #This is for creating accounts
@@ -37,6 +51,14 @@ def deposit(self, amount):
         print(f"Deposited ${amount}. New balance: ${self.balance}")
     else:
         print("Invalid deposit amount.")
+# This is for the transfer method
+def transfer(self,target_account, amount):
+    if amount > 0 and amount <= self.balance:
+        self.balance -= amount
+        target_account.balance += amount
+        print(f"Transferred ${amount} to Account {target_account.account_number}. New balance: ${self.balance}")
+    else:
+        print("Invalid transfer amount or insufficient funds.")
 # this is for the withdraw method
 def withdraw(self, amount):
     if 0 < amount <= self.balance:
