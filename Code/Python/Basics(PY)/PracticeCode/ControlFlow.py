2c7b6f3dfc9a2628ec3f56917548eb93768b704b
Anthony <102490646+Drakeze@users.noreply.github.com>
2025-05-20 11:59:14 -0700

    made the options

--- Code/Python/Basics(PY)/PracticeCode/ControlFlow.py
+++ Code/Python/Basics(PY)/PracticeCode/ControlFlow.py
@@ -1,4 +1,5 @@
-# this is a ATM Machine Simulator
+#This is a ATM Machine Simulator
+balance = 1000
 def atm():
     print("Welcome to the ATM Machine")
 
@@ -12,3 +13,26 @@ def atm():
     if pin == "12345":
         print("Pin accepted")
         print("Please select an option")
+# The options for the user
+check_balance = 1
+deposit = 2
+withdraw = 3
+transfer = 4
+quit = 5
+if check_balance == 1:
+    print(f"Your balance is {balance}")
+if deposit == 2:
+    amount = int(input("Please enter the amount you want to deposit"))
+    balance += amount
+    print(f"Your new balance is {balance}")
+if withdraw == 3:
+    amount = int(input("Please enter the amount you want to withdraw"))
+    balance -= amount
+    print(f"Your new balance is {balance}")
+if transfer == 4:
+    amount = int(input("Please enter the amount you want to transfer"))
+    balance -= amount
+    print(f"Your new balance is {balance}")
+if quit == 5:
+    print("Thank you for using the ATM Machine")
+    return
